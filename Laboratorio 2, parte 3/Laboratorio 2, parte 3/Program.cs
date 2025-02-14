cd9c53f22f2be2c0028132330c02b1f69bcb0df8
Alejandro Godoy Campos <egodoyc4@miumg.edu.gt>
2025-02-14 02:01:27 -0600

    Add files via upload

---
+++ Laboratorio 2, parte 3/Laboratorio 2, parte 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+double valor1, valor2;
+
+Console.WriteLine("Introduce el primer valor numérico:");
+valor1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Introduce el segundo valor numérico:");
+valor2 = Convert.ToDouble(Console.ReadLine());
+
+if (valor1 > valor2)
+{
+    Console.WriteLine($"{valor1} es mayor que {valor2}");
+}
+else if (valor1 < valor2)
+{
+    Console.WriteLine($"{valor1} es menor que {valor2}");
+}
+else
+{
+    Console.WriteLine($"{valor1} es igual a {valor2}");
+}
+
+// Acá hacemos las comparaciones
+Console.WriteLine($"¿{valor1} es mayor que {valor2}? {valor1 > valor2}");
+Console.WriteLine($"¿{valor1} es menor que {valor2}? {valor1 < valor2}");
+Console.WriteLine($"¿{valor1} es igual a {valor2}? {valor1 == valor2}");
+
+bool andResult = (valor1 > 0) && (valor2 > 0);
+bool orResult = (valor1 > 0) || (valor2 > 0);
+
+Console.WriteLine($"¿Ambos valores son mayores que 0? {andResult}");
+Console.WriteLine($"¿Alguno de los valores es mayor que 0? {orResult}");
